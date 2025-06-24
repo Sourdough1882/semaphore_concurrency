4f194bf978872ce76dae3cac9baa4087f956facd
Valery Inogamov <sourdough1882@gmail.com>
2025-06-24 18:06:33 +1000

    Small refactor and add new features
    - Restructured main function for better readability
    - Added optional timeout parameter to WebsiteScreenshotter class
    - Removed url default naming and provided new parameter to
    screenshot function

--- scraper_with_limit.py
+++ scraper_with_limit.py
@@ -1,20 +1,25 @@
+import time
+
 import asyncio
-from urllib.parse import urlencode
 from playwright.async_api import async_playwright
+
+from urllib.parse import urlencode
 
 
 class WebsiteScreenshotter:
-    def __init__(self, context_parameters: dict, concurrency: int = 3) -> None:
+    def __init__(self, context_parameters: dict, concurrency: int = 3, timeout: int = 30000) -> None:
         self.semaphore = asyncio.Semaphore(concurrency)
         self.context_parameters = context_parameters
+        self.timeout = timeout
         self.playwright = None
         self.browser = None
         self.context = None
 
     async def __aenter__(self):
         self.playwright = await async_playwright().start()
-        self.browser = await self.playwright.chromium.launch()
+        self.browser = await self.playwright.chromium.launch(headless=False)
         self.context = await self.browser.new_context(**self.context_parameters)
+        self.context.set_default_timeout(self.timeout)
         return self
 
     async def __aexit__(self, *args):
@@ -25,13 +30,13 @@ class WebsiteScreenshotter:
         if self.context is not None:
             await self.context.close()
 
-    async def screenshot(self, url):
+    async def screenshot(self, url, filename):
         async with self.semaphore:
             if self.context is not None:
                 page = await self.context.new_page()
                 try:
                     await page.goto(url)
-                    await page.screenshot(path=f"screenshot_{url}.jpg")
+                    await page.screenshot(path=f"{filename}.jpg", full_page=True)
                 finally:
                     await page.close()
 
@@ -41,23 +46,17 @@ async def main():
 
     page_count = 21
 
-    url_list = []
+    query_string = 'видеокарта rtx 5070'
 
-    for count in range(1, page_count):
-        query_params: dict = {
-            'p': count,
-            'q': 'видеокарта rtx 5070'
-        }
-
-        query_string: str = urlencode(query_params)
-
-        url_list.append(f"{base_url}?{query_string}")
+    url_list = [f"{base_url}?{urlencode({'p': count, 'q': query_string})}"
+                for count in range(1, page_count)]
 
     context_parameters = {
         'user_agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
 
-    async with WebsiteScreenshotter(context_parameters=context_parameters, concurrency=3) as screenshotter:
-        tasks = [screenshotter.screenshot(url) for url in url_list]
+    async with WebsiteScreenshotter(context_parameters=context_parameters, concurrency=3, timeout=100000) as screenshotter:
+        tasks = [screenshotter.screenshot(
+            url, f"screenshot/avito_screenshot_{url_list.index(url)}") for url in url_list]
 
         await asyncio.gather(*tasks)
 
